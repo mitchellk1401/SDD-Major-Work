b166b3230ea3a4af97759f70f81b9a9cda7e0646
mitchellk1401 <mitchellk1401@gmail.com>
2019-10-23 13:08:37 +1100

    Added Unity 2d Project WIth Basic Character Model In Aesprite

---
+++ SDD Major Work Unity/Assets/playerMovement.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerMovement : MonoBehaviour
+{
+
+    public float speed;
+    public Object player;
+  
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
