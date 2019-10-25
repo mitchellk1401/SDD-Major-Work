cb6167d5dc6c803c330821766b0cd8262d88b490
mitchellk1401 <47933464+mitchellk1401@users.noreply.github.com>
2019-10-25 17:35:26 +1100

    Added Basic Movement Script

---
+++ SDD Major Work Unity/Assets/Assets/Scripts/playerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour
+{
+    public float moveSpeed = 4f;
+    public Rigidbody2D rb;
+
+    Vector2 movePos;
+
+  
+    void Update()
+    {
+        movePos.x = Input.GetAxisRaw("Horizontal");
+        movePos.y = Input.GetAxisRaw("Vertical");
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movePos * moveSpeed * Time.deltaTime);
+    }
+}
