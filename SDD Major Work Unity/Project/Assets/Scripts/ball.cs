17f4ce68044cbb498de770237f1bf90c01072941
mitchellk1401 <mitchellk1401@gmail.com>
2019-11-13 12:26:22 +1100

    Working Tilemap colliders kind of, corners broken

--- SDD Major Work Unity/Project/Assets/Scripts/ball.cs
+++ SDD Major Work Unity/Project/Assets/Scripts/ball.cs
@@ -8,7 +8,12 @@ public class ball : MonoBehaviour
 
     private void OnCollisionEnter2D(Collision2D collision)
     {
-
+        if(collision.gameObject.tag == "Player")
+        {
+            throwing.ammo += 1;
+            Destroy(gameObject);
+        }
+     
 
     }
 }
