7311b37d7f58f556ecc8d70e80b6ca6fd9c4f009
mitchellk1401 <mitchellk1401@gmail.com>
2019-11-22 12:00:34 +1100

    Added enemy patrol script (working) + adjusted bounce

--- SDD Major Work Unity/Project/Assets/Scripts/enemyController.cs
+++ SDD Major Work Unity/Project/Assets/Scripts/enemyController.cs
@@ -4,22 +4,35 @@ using UnityEngine;
 
 public class enemyController : MonoBehaviour
 {
-    public GameObject enemy;
-    public GameObject player;
-    public Rigidbody2D rb;
+    public Transform[] movePositions;
 
     public float speed;
-    public float health;
-    private float maxHealth = 100f;
-    void Start()
-    {
-        health = maxHealth;
-    }
+    private float waitTime;
+    private int randomPos;
 
+     void Start()
+    {
+        waitTime = 2f;
+        randomPos = Random.Range(0, movePositions.Length);        
+        
+    }
 
     void Update()
     {
-
+        transform.position = Vector2.MoveTowards(transform.position, movePositions[randomPos].position, speed * Time.deltaTime);
+        if (Vector2.Distance(transform.position, movePositions[randomPos].position) < 2)
+        {
+            if(waitTime<= 0)
+            {
+                randomPos = Random.Range(0, movePositions.Length);
+                waitTime = 2;
+            }
+            else
+            {
+                waitTime -= Time.deltaTime;
+            }
+        }
     }
 
-}
+
+}   
