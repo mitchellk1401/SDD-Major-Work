3cd361a89398d3446e4cfdc8f546aa4ec107de97
mitchellk1401 <mitchellk1401@gmail.com>
2020-01-30 10:38:53 +1100

    Added sprint Controls + added fog as  a scene, + Changed map

--- 2D Platformer/Scripts/KinematicBody2D.gd
+++ 2D Platformer/Scripts/KinematicBody2D.gd
@@ -5,24 +5,31 @@ const GRAVITY = 10
 const ACCELERATION = 50
 const MAX_SPEED = 300
 const JUMP_HEIGHT = -350
+var Sprint = 1
+
 
 var motion = Vector2() 
 
 #use $ to access child objects
-
 	
 func _physics_process(delta):
-	
 	var friction = false
 	
+	# Sprint Modifier -- "ui_sprint" is a custom input for SHIFT key
+	if Input.is_action_just_pressed("ui_sprint"):
+		Sprint = 1.4
+	if Input.is_action_just_released("ui_sprint"):
+		Sprint = 1
+
+	#Motion Controls
 	if Input.is_action_pressed("ui_right"):		
-		motion.x = min(motion.x + ACCELERATION , MAX_SPEED)
+		motion.x = min(motion.x + ACCELERATION , MAX_SPEED * Sprint)
 		$Sprite.flip_h = false
 		$Sprite.play("Run")
 		
 		
 	elif Input.is_action_pressed("ui_left"):
-		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED)
+		motion.x = max(motion.x - ACCELERATION, -MAX_SPEED * Sprint)
 		$Sprite.flip_h = true
 		$Sprite.play("Run")
 		
@@ -31,18 +38,19 @@ func _physics_process(delta):
 		$Sprite.play("Idle")
 		friction = true
 
+	# Check if player is on floor and play correct motion
 	if is_on_floor():
-		if Input.is_action_just_pressed("ui_up"):
+		if Input.is_action_pressed("ui_up"):
 			motion.y = JUMP_HEIGHT
 		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.9)
+			motion.x = lerp(motion.x, 0, 0.9) #make movement smoother
 	else:
 		if motion.y > 0:
 			$Sprite.play("Jump")	
 		else:
 			$Sprite.play("Fall")
 		if friction == true:
-			motion.x = lerp(motion.x, 0, 0.1)
+			motion.x = lerp(motion.x, 0, 0.1) #make movement smoother
 	
 	motion.y += GRAVITY
 	
