13b2365142494e2c0deab4b28d820d3aa7d327d7
mitchellk1401 <47933464+mitchellk1401@users.noreply.github.com>
2020-04-08 09:01:33 +1000

    Added Cannon Obsticle (fully animated) + Can work directly with inspector based on object orientation

---
+++ 2D Platformer/Scripts/Cannon.gd
@@ -0,0 +1,28 @@
+extends Node2D
+
+var frame = 0
+var player = null
+var scene = load("res://Scenes/ObjectScenes/CannonBall.tscn")
+var shot = false
+var timer = null
+
+
+func _physics_process(delta):
+	var player = scene.instance()
+	frame = $AnimatedSprite.get_frame()
+	if frame == 5 && shot != true:
+		shot = true
+		cannonShoot()
+		$ShootingPoint.add_child(player)
+
+func cannonShoot():
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(0.3)
+	timer.connect("timeout", self, "canShoot")
+	add_child(timer)
+	timer.start()
+	return  
+
+func canShoot():
+	shot = false
---
+++ 2D Platformer/Scripts/CannonBall.gd
@@ -0,0 +1,62 @@
+extends KinematicBody2D
+
+var rotationCannon = 0
+var projSpeed = 3
+var bodies = null
+var collide = null
+var direction = Vector2()
+var timer = null
+var cannonSprite = null
+
+func _ready():
+	var cannon = get_parent().get_parent()
+	rotationCannon = cannon.get_rotation_degrees()
+	rotationCannon = int(round(rotationCannon)) # Small Chance the godot engine sets the variable as a float so make it a rounded int to avoid errors
+	
+	#TESTING
+	cannonSprite = get_child(0)
+	cannonSprite.set_position(-3 * direction)
+	
+		# As there is no way to change the rotation of a kinematic body, If statements are currently the best option
+	if rotationCannon == 0:
+		direction.x = projSpeed
+		direction.y = 0	
+	elif rotationCannon == 90:
+		direction.x = 0
+		direction.y = projSpeed
+	elif rotationCannon == 180:
+		direction.x = -projSpeed
+		direction.y = 0
+	elif rotationCannon == 270:
+		direction.x = 0
+		direction.y = -projSpeed
+	
+	
+	
+func _physics_process(delta):	 
+	var bodies = $Area2D.get_overlapping_bodies()
+	
+
+
+		
+	
+	var collide = move_and_collide(direction)
+	
+	for body in bodies:
+		if body.name == "Player":
+			get_tree().reload_current_scene()
+	
+	if collide: # Collide true with any collision (built into move_and_collide)
+		deleteDelay()
+
+func deleteDelay(): # Allows Player to be noticed before deleting
+	timer = Timer.new()
+	timer.set_one_shot(true)
+	timer.set_wait_time(0.05)
+	timer.connect("timeout", self, "Delete")
+	add_child(timer)
+	timer.start()
+	return  
+
+func Delete():
+	queue_free()
